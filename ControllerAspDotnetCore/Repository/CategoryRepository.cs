f03d0b438a002f7d9f20711f8a9ef77cbe8924cd
zaintahiri <zain.tahiri940@gmail.com>
2024-12-21 11:35:33 +0500

    Static CategoryRepository class added with static in memory list object that will hold the data. and operations required to apply on in memory llist object are created with functionality

---
+++ ControllerAspDotnetCore/Repository/CategoryRepository.cs
@@ -0,0 +1,53 @@
+﻿using ControllerAspDotnetCore.Models;
+
+namespace ControllerAspDotnetCore.Repository
+{
+    public static class CategoryRepository
+    {
+        private static List<Category> _categories = new List<Category> {
+            new Category{ Id=1,Name="Beverage",Description="Beverage Category"},
+            new Category{ Id=2,Name="Bakery",Description="Bakery Category"},
+            new Category{ Id=3,Name="Meat",Description="Meat Category"},
+        };
+
+        public static void AddCategory(Category category)
+        {
+            var _maxId=_categories.Max(x => x.Id);
+            category.Id = _maxId;
+            _categories.Add(category);
+        
+        }
+
+        public static List<Category> GetCategories() => _categories;
+
+        public static Category? GetCategory(int categoryId)
+        { 
+            var category = _categories.FirstOrDefault(x=>x.Id==categoryId);
+            if (category != null)
+            { 
+                return new Category { Id=category.Id,Name=category.Name,Description=category.Description};
+            }
+
+            return null;
+        }
+
+        public static void UpdateCategory(int categoryId, Category category)
+        {
+            var categoryToUpdate = GetCategory(categoryId);
+            if (categoryToUpdate != null)
+            { 
+                categoryToUpdate.Name=category.Name;
+                categoryToUpdate.Description=category.Description;  
+            }
+        }
+
+        public static void DeleteCategory(int categoryId)
+        {
+            var cat = _categories.FirstOrDefault(x => x.Id == categoryId);
+            if (cat != null)
+            { 
+                _categories.Remove(cat);
+            }
+        }
+    }
+}
